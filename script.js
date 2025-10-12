1f8c0084c65a7a683c8facce9a2f6e9e6cdfce7a
kssb2156 <kssb2156@gmail.com>
2025-10-13 00:28:22 +0700

    script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Sample period data - you'd replace this with real data from a backend or local storage
     const periodData = [
+        { start: new Date(2025, 10, 12), end: new Date(2025, 10, 17) }, /* สำหรับแก้ไขวันที่ 0 คือเดือน Jan */
         { start: new Date(2025, 8, 7), end: new Date(2025, 8, 10) }, /* สำหรับแก้ไขวันที่ 0 คือเดือน Jan */
         { start: new Date(2025, 7, 4), end: new Date(2025, 7, 9) },
         { start: new Date(2025, 5, 23), end: new Date(2025, 5, 28) },
